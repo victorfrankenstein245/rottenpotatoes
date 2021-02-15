098861a93019789d499e4e9c6ce951c3f9cabf9c
victorfrankenstein245 <56684147+victorfrankenstein245@users.noreply.github.com>
2021-02-15 15:36:12 +0100

    js file changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movies?sort_by=popularity.desc&api_key=19698c99c99fd37101050cfd756f5f66&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=19698c99c99fd37101050cfd756f5f66&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=19698c99c99fd37101050cfd756f5f66&qwerry='";
+  "https://api.themoviedb.org/3/search/movie?api_key=19698c99c99fd37101050cfd756f5f66&query='";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -20,25 +20,25 @@ async function getMovies(url) {
 function showMovies(movies) {
   main.innerHTML = "";
 
-  movies.forEach((movie) => {
+  movies.forEeach((movie) => {
     const { title, poster_path, vote_average, overviwe } = movie;
 
     const movieEL = document.createElement("div");
-    movieEL.classslist.add("movie");
+    movieEL.classList.add("movie");
 
     movieEL.innerHTML = `
         <div class="movie"> <img src="${IMG_PATH + poster_path}" alt="${title}">
-            <div class="movie-info">
+            
                 <h3>${title}</h3>
                 <span class="${getClassByRate(
                   vote_average
                 )}">${vote_average}</span>
-            </div>
-            <div class="overview">
+               </div>
+               <div class="overview">
                 <h3>overview</h3>
                ${overviwe}
 
-            </div>
+            
         </div>
         `;
     main.appendChild(movieEL);
